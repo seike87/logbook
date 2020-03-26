096ad54335342826459208fcdb448e75228b3341
seike <seike@posteo.de>
2020-03-26 09:43:52 +0100

    Finished Design of the Model

--- log/models.py
+++ log/models.py
@@ -6,12 +6,14 @@ class Logs(models.Model):
     datum = models.DateTimeField(default=timezone.now)
     name = models.CharField('Name', max_length=100)
     vorname = models.CharField('Vorname', max_length=100)
-    ziel = models.TextField('Ziel')
+    ziel = models.CharField('Ziel', max_length=500)
     grund = models.CharField('Grund', max_length=200)
-    von = models.DateField('Von')
-    bis = models.DateField('Bis')
-    kostenstelle = models.CharField('Kostenstelle', max_length=50)
+    von = models.TimeField('Von')
+    bis = models.TimeField('Bis')
+    kostenstelle = models.CharField('Kostenstelle', max_length=12)
     projekt = models.CharField('Projekt', max_length=200)
 
+
+
     def __str__(self):
-        return self.title
+        return self.grund
